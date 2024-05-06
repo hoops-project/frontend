aad9aa25bba796858f0e486e61bd2e7cfa6a8fc4
osw6858 <osw7875@naver.com>
2024-05-06 23:05:30 +0900

    feat: MSW 기본 셋팅 완료

--- src/main.tsx
+++ src/main.tsx
@@ -8,15 +8,27 @@ import { queryClient } from './api/QueryClient.ts'
 import { ThemeProvider } from 'styled-components'
 import { theme } from './styles/theme.ts'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <ThemeProvider theme={theme}>
-        <GlobalStyle />
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </ThemeProvider>
-    </QueryClientProvider>
-  </React.StrictMode>
-)
+async function enableMocking() {
+  if (!import.meta.env.DEV) {
+    return
+  }
+
+  const { worker } = await import('./mock/setupWorker.ts')
+
+  return worker.start({ onUnhandledRequest: 'bypass' })
+}
+
+enableMocking().then(() => {
+  ReactDOM.createRoot(document.getElementById('root')!).render(
+    <React.StrictMode>
+      <QueryClientProvider client={queryClient}>
+        <ThemeProvider theme={theme}>
+          <GlobalStyle />
+          <BrowserRouter>
+            <App />
+          </BrowserRouter>
+        </ThemeProvider>
+      </QueryClientProvider>
+    </React.StrictMode>
+  )
+})
