445ccf2d77b4f1350eec4065f5f14c71baf4e5e4
osw6858 <osw7875@naver.com>
2024-05-08 19:13:09 +0900

    refactor: 임시 데이터 코드 분리

--- src/components/MyGameList/MyGameList.tsx
+++ src/components/MyGameList/MyGameList.tsx
@@ -4,6 +4,7 @@ import React, { useEffect, useState } from 'react'
 import { Pagination, Stack } from '@mui/material'
 import GameUserList from '../GameUserList/GameUserList.tsx'
 import { useParams } from 'react-router-dom'
+import { TestData, TestData2 } from '../../hooks/temp.ts'
 
 export default function MyGameList() {
   const [selected, setSelected] = useState<number>(0)
@@ -20,55 +21,6 @@ export default function MyGameList() {
   useEffect(() => {
     // console.log(params.id)
   }, [params.id])
-
-  const TestData = [
-    {
-      gameId: 1,
-      title: '강남 농구장',
-      startDateTime: '2020.1.1',
-    },
-    {
-      gameId: 2,
-      title: '노원 농구장',
-      startDateTime: '2020.2.1',
-    },
-    {
-      gameId: 3,
-      title: '한강 농구장',
-      startDateTime: '2020.2.1',
-    },
-    {
-      gameId: 4,
-      title: '잠실 농구장',
-      startDateTime: '2020.2.1',
-    },
-    {
-      gameId: 5,
-      title: '의정부 농구장',
-      startDateTime: '2020.2.1',
-    },
-  ]
-
-  const TestData2 = [
-    {
-      userId: 1,
-      gameId: 1,
-      name: '오신웅',
-      rate: 4.5,
-    },
-    {
-      userId: 2,
-      gameId: 1,
-      name: '김영찬',
-      rate: 4.5,
-    },
-    {
-      userId: 3,
-      gameId: 1,
-      name: '이솔',
-      rate: 4.5,
-    },
-  ]
 
   return (
     <S.Wrapper>
