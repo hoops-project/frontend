ecc2e30fde87b3ef6ccdd5b6dfffa70fa84e95d8
Pinn <lsol1130@naver.com>
2024-05-22 21:11:52 +0900

    feat: 로그아웃 및 회원탈퇴 기능 구현

--- src/components/Notification/Notification.tsx
+++ src/components/Notification/Notification.tsx
@@ -2,14 +2,45 @@ import { S } from './Notification.style.ts'
 import noticeBell from '../../assets/fluent-emoji-flat_bell.svg'
 import { CS } from '../../styles/commonStyle.ts'
 import { PiInfoLight } from 'react-icons/pi'
+import { END_POINT } from '../../constants/endPoint.ts'
+import { useUserInfoQuery } from '../../hooks/query/useUserInfoQuery.ts'
+import { axiosAccess } from '../../api/axiosInstance.ts'
+import { useNavigate } from 'react-router-dom'
+import useDeactivateQuery from '../../hooks/query/useDeActivateQuery.ts'
+import useToast from '../../hooks/useToast.ts'
 
 export default function Notification() {
+  const { toastSuccess } = useToast()
+  const navigate = useNavigate()
+  const { userInfo } = useUserInfoQuery()
+  const { deactivateMutate } = useDeactivateQuery()
+
+  const handleSignOut = async () => {
+    const checkLogout = window.confirm('로그아웃을 하시겠습니까?')
+    if (checkLogout) {
+      await axiosAccess.post(`${END_POINT.AUTH.LOGOUT}`).then(() => {
+
+        localStorage.removeItem("Access-Token")
+      });
+      toastSuccess('로그아웃이 성공적으로 완료되었습니다 💪🏻')
+      navigate("/", { replace: true })
+    }
+  };
+
+  const handleDeactivate = () => {
+    const checkWithdraw = window.confirm("정말 회원 탈퇴를 하시겠습니까?")
+    if (checkWithdraw) {
+      deactivateMutate()
+    }
+  };
+
+
   return (
     <S.Wrapper>
       <S.UserInfo>
-        <p>오신웅</p>
+        <p>{userInfo?.name}</p>
         <div>
-          <span>시눙하이</span> <span>1996.03.28</span>
+          <span>{userInfo?.nickname}</span> <span>{userInfo?.birthday}</span>
         </div>
       </S.UserInfo>
       <S.NoticeTitle>
@@ -26,6 +57,9 @@ export default function Notification() {
           </CS.Link>
         ))}
       </S.NoticeBody>
+      <S.Logout>
+        <span onClick={handleSignOut}>로그아웃</span> <span>|</span> <span onClick={handleDeactivate}>회원탈퇴</span>
+      </S.Logout>
     </S.Wrapper>
   )
 }
