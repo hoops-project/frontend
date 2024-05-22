054a502e48aa1890f1a008536b52c40db90c5c55
Pinn <lsol1130@naver.com>
2024-05-22 19:40:42 +0900

    fix: 랜더링이 겹치는 문제 해결

--- src/routes/PrivateRoute.tsx
+++ src/routes/PrivateRoute.tsx
@@ -1,17 +1,24 @@
-import { Navigate, Outlet } from 'react-router-dom'
-import { useToast } from '../hooks/useToast'
+import React, { useEffect } from 'react';
+import { Outlet, useNavigate } from 'react-router-dom';
+import { useToast } from '../hooks/useToast';
 
+const PrivateRoute: React.FC = () => {
+  const { toastError } = useToast();
+  const navigate = useNavigate();
+  const isAuthenticated = localStorage.getItem('Access-Token');
 
-const PrivateRoute = () => {
-  const { toastError } = useToast()
-  const isAuthenticated = localStorage.getItem('Access-Token')
-  
+  useEffect(() => {
     if (!isAuthenticated) {
-      toastError('로그인을 진행해주세요!')
-      return <Navigate to= '/sign-in' />
+      toastError('로그인을 진행해주세요!');
+      navigate('/sign-in');
     }
-    return <Outlet />
+  }, [isAuthenticated, toastError, navigate]);
+
+  if (!isAuthenticated) {
+    return null;
   }
 
+  return <Outlet />;
+};
 
 export default PrivateRoute;
