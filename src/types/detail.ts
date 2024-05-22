2719efa6a4c5ed2147f14ec6e3faa31766292849
osw6858 <osw7875@naver.com>
2024-05-22 20:41:46 +0900

    feat: 게임 디테일 응답값 타입 지정 및 추가

--- src/types/detail.ts
+++ src/types/detail.ts
@@ -1,5 +1,28 @@
-export interface DetailProps {
-  match: {
-    title: string
-  }
+export interface ParticipantUser {
+  userId: number
+  genderType: string
+  nickName: string
+  playStyle: string
+  ability: string
+}
+
+export interface GameDetails {
+  address: string
+  cityName: string
+  content: string
+  createdDateTime: string
+  deletedDateTime: string | null
+  fieldStatus: string
+  gameId: number
+  gender: string
+  headCount: number
+  inviteYn: boolean
+  latitude: number
+  longitude: number
+  matchFormat: string
+  nickName: string
+  participantUserList: ParticipantUser[]
+  startDateTime: string
+  title: string
+  userId: number
 }
