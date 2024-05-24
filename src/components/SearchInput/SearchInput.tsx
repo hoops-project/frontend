68c4cfa47678a5ba06b5ecf6b6a0bed4d4b1dd49
osw6858 <osw7875@naver.com>
2024-05-24 11:54:31 +0900

    feat: 검색 디바운스 처리를 위한 커스텀훅 추가

--- src/components/SearchInput/SearchInput.tsx
+++ src/components/SearchInput/SearchInput.tsx
@@ -3,9 +3,11 @@ import SearchIcon from '../../assets/search-icon.svg'
 
 export default function SearchInput() {
   return (
-    <S.InputContainer>
-      <S.SearchIcon src={SearchIcon} alt='Search icon' />
-      <S.Search placeholder='지역, 모임 이름으로 검색' />
-    </S.InputContainer>
+    <>
+      <S.InputContainer>
+        <S.SearchIcon src={SearchIcon} alt='Search icon' />
+        <S.Search placeholder='지역, 모임 이름으로 검색' />
+      </S.InputContainer>
+    </>
   )
 }
