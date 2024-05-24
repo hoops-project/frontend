cc4fb1532568882a9699106d047780100d65bd72
osw6858 <osw7875@naver.com>
2024-05-24 12:16:51 +0900

    feat: 디바운스를 적용한 검색 쿼리 추가

--- src/components/SearchInput/SearchInput.tsx
+++ src/components/SearchInput/SearchInput.tsx
@@ -1,13 +1,29 @@
 import { S } from './SearchInput.style.ts'
 import SearchIcon from '../../assets/search-icon.svg'
+import { useState } from 'react'
+import { useDebounce } from '../../hooks/useDebounce.ts'
+import { useSearchGameQuery } from '../../hooks/query/useSearchGameQuery.ts'
 
 export default function SearchInput() {
+  const [searchValue, setSearchValue] = useState<string>('')
+
+  const query = useDebounce(searchValue, 500)
+
+  const { searchResult } = useSearchGameQuery(query)
+
+  console.log(searchResult)
+
   return (
     <>
       <S.InputContainer>
         <S.SearchIcon src={SearchIcon} alt='Search icon' />
-        <S.Search placeholder='지역, 모임 이름으로 검색' />
+        <S.Search
+          placeholder='지역으로 경기 검색'
+          value={searchValue}
+          onChange={(e) => setSearchValue(e.target.value)}
+        />
       </S.InputContainer>
+      {query && <div>{query}</div>}
     </>
   )
 }
