b49b7b884054b711df7519d8672cdd2964aa2d97
Pinn <lsol1130@naver.com>
2024-05-26 19:35:13 +0900

    fix:refreshToken 함수 구동 오류로 인한 intercepter분리

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -25,6 +25,43 @@ export const axiosAccess = axios.create({
   withCredentials: true,
 });
 
+export const axiosRequest = axios.create({
+  baseURL: '/api',
+  headers: {
+    "Content-Type": "application/json",
+  },
+  withCredentials: true,
+});
+
+const refreshToken = async (req: AxiosRequestConfig) => {
+  try {
+    const refreshToken = localStorage.getItem("Refresh-Token");
+    if (!refreshToken) {
+      throw new Error("Refresh token not available");
+    }
+
+    const res = await axiosRequest.post(`${END_POINT.AUTH.REFRESH_TOKEN}`);
+    
+    const newACToken = res.data.accessToken; // 보통 access token은 body
+    localStorage.setItem("Access-Token", newACToken);
+
+    // headers가 undefined인 경우 초기화
+    if (!req.headers) {
+      req.headers = {};
+    }
+
+    req.headers.Authorization = `Bearer ${newACToken}`;
+    return await axios(req);
+  } catch (err) {
+    alert('로그인을 다시 진행해주세요!')
+    window.location.replace("/sign-in");
+    localStorage.removeItem('Access-Token');
+    localStorage.removeItem('Refresh-Token');
+    throw err;
+  }
+};
+
+
 // 일반적으로 인증이 필요한 API 요청을 처리 => 요청 시 Authorization 헤더를 설정
 axiosAuth.interceptors.request.use(
   (config) => {
@@ -40,37 +77,6 @@ axiosAuth.interceptors.request.use(
   }
 );
 
-const refreshToken = async (req: AxiosRequestConfig) => {
-  try {
-    const refreshToken = localStorage.getItem("Refresh-Token");
-    if (!refreshToken) {
-      throw new Error("Refresh token not available");
-    }
-
-    const res = await axiosAccess.post(`${END_POINT.AUTH.REFRESH_TOKEN}`,{
-      headers: {
-        'Authorization': `Bearer ${refreshToken}`
-      }
-    });
-    
-    const newACToken = res.data.accessToken; // 보통 access token은 body
-    localStorage.setItem("Access-Token", newACToken);
-
-    // headers가 undefined인 경우 초기화
-    if (!req.headers) {
-      req.headers = {};
-    }
-
-    req.headers.Authorization = `Bearer ${newACToken}`;
-    return await axiosAuth(req);
-  } catch (err) {
-    alert('로그인을 다시 진행해주세요!')
-    localStorage.removeItem('Access-Token');
-    localStorage.removeItem('Refresh-Token');
-    throw err;
-  }
-};
-
 // access-token 만료시 refresh-token 사용해서 재발급
 axiosAuth.interceptors.response.use(
   (response) => response,
@@ -82,16 +88,20 @@ axiosAuth.interceptors.response.use(
     if (errorCode === "EXPIRED_TOKEN") {
       return refreshToken(req);
     }
-    return Promise.reject(error('AUTH 리퀘스트 에러'));
+    return Promise.reject(error);
   }
 );
 
 // 주로 토큰 재발급 요청 처리 => 요청 시 Access-Token 헤더를 설정하며, 토큰이 만료되었을 때 새로운 토큰
+// 인증 인가에 토큰을 2개를 필요로 하는 API요청에 사용!
+
 axiosAccess.interceptors.request.use(
   (config) => {
     const accessToken = localStorage.getItem('Access-Token');
+    const refreshToken = localStorage.getItem('Refresh-Token');
     if (accessToken) {
       config.headers.Authorization = `Bearer ${accessToken}`;
+      config.headers.refreshToken = `${refreshToken}`;
     }
     return config;
   },
@@ -110,7 +120,37 @@ axiosAccess.interceptors.response.use(
 
     if (errorCode === "EXPIRED_TOKEN") {
       return refreshToken(req);
+
     }
     return Promise.reject(error('ACCESS 리스폰스 에러'));
   }
 );
+
+axiosRequest.interceptors.request.use(
+  (config) => {
+    const accessToken = localStorage.getItem("Refresh-token");
+    if (accessToken) {
+      config.headers.Authorization = `Bearer ${accessToken}`;
+    }
+    return config;
+  },
+  (error) => {
+    console.log(error);
+    return Promise.reject(error);
+  }
+);
+
+axiosRequest.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    console.log(error);
+    const errorCode = error.response.data.errorCode;
+    const req = error.config;
+
+    if (errorCode) {
+      return console.log(error);
+    }
+    return Promise.reject(error);
+  }
+);
+
