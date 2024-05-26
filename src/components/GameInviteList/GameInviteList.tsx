d39d4adbf0a1de4b0c98b81ab2e3c7764e497a5d
osw6858 <osw7875@naver.com>
2024-05-27 01:29:23 +0900

    feat: 경기 신청 리스트 쿼리 작성

--- src/components/GameInviteList/GameInviteList.tsx
+++ src/components/GameInviteList/GameInviteList.tsx
@@ -1,13 +1,58 @@
 import { S } from '../FriendRequestList/FriendRequestList.style.ts'
-import RequestItem from '../common/RequestItem/RequestItem.tsx'
+import { useGameInviteQuery } from '../../hooks/query/useGameInviteQuery.ts'
+import { Invite } from '../../types/invite.ts'
+import { PiInfoLight } from 'react-icons/pi'
+import useModal from '../../hooks/useModal.ts'
+import BasicButton from '../common/BasicButton/BasicButton.tsx'
+import { theme } from '../../styles/theme.ts'
+import Modal from '../common/Modal/Modal.tsx'
+import ModalTit from '../common/ModalTit/ModalTit.tsx'
 
 export default function GameInviteList() {
+  const { inviteList } = useGameInviteQuery()
+  const { isModalOpen, openModal, closeModal } = useModal()
+
+  console.log(inviteList)
+
   return (
     <S.Wrapper>
       <p>경기 초대 목록</p>
-      {Array.from({ length: 6 }, (_, index) => (
-        <RequestItem key={index} />
+      {inviteList?.map((info: Invite) => (
+        <S.Invite key={info.gameId}>
+          <p>{info.senderUserId}</p>
+          <PiInfoLight onClick={openModal} />
+          <S.ButtonWrapper>
+            <BasicButton
+              type={'button'}
+              $bgColor={theme.colors.blue}
+              $fontcolor={theme.colors.white}
+              $width={'6rem'}
+            >
+              수락
+            </BasicButton>
+            <BasicButton
+              type={'button'}
+              $bgColor={theme.colors.red}
+              $fontcolor={theme.colors.white}
+              $width={'6rem'}
+            >
+              거절
+            </BasicButton>
+          </S.ButtonWrapper>
+        </S.Invite>
       ))}
+      {isModalOpen && (
+        <Modal $width='50rem' $height='30rem' onClose={closeModal}>
+          <ModalTit title='장소 정보' />
+          <BasicButton
+            type='button'
+            children={'닫기'}
+            $fontcolor={theme.colors.white}
+            $bgColor={theme.colors.blue}
+            onClick={closeModal}
+          />
+        </Modal>
+      )}
     </S.Wrapper>
   )
 }
