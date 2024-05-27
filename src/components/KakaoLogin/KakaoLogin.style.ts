4682bdd1ad315d2b6b11481c6d8ef18769252bdc
Pinn <lsol1130@naver.com>
2024-05-28 00:37:51 +0900

    fix: 카카오 로그인 리다이렉트를 위한 기능 구현

---
+++ src/components/KakaoLogin/KakaoLogin.style.ts
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const S ={ 
+  OAuthTitle: styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`,
+KakaoIcon: styled.img`
+  margin-right: 1rem;
+`,
+}
