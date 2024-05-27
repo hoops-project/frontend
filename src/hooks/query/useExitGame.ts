2755d3ed5407081abed899bb73bd4874ad3e14a2
osw6858 <osw7875@naver.com>
2024-05-27 16:49:27 +0900

    hotfix: vercel.json 설정 파일 추가

--- src/hooks/query/useExitGame.ts
+++ src/hooks/query/useExitGame.ts
@@ -1,8 +1,10 @@
 import { axiosAuth } from '../../api/axiosInstance.ts'
 import { END_POINT } from '../../constants/endPoint.ts'
-import { useMutation } from '@tanstack/react-query'
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+import { QUERY_KEYS } from '../../constants/queryKeys.ts'
 
 export const useExitGame = () => {
+  const queryClient = useQueryClient()
   const handleExitRoom = async (gameId: number) => {
     const { data } = await axiosAuth.patch(END_POINT.GAME_CREATOR.GAME_DELETE, {
       gameId,
@@ -10,7 +12,13 @@ export const useExitGame = () => {
     return data
   }
 
-  const { mutate: exitGameMutate } = useMutation({ mutationFn: handleExitRoom })
+  const { mutate: exitGameMutate } = useMutation({
+    mutationFn: handleExitRoom,
+    onSuccess: () =>
+      queryClient.invalidateQueries({
+        queryKey: [QUERY_KEYS.GET_GAME_LIST],
+      }),
+  })
 
   return { exitGameMutate }
 }
