7be60300a40065f7487a98a68674f32effeafcff
osw6858 <osw7875@naver.com>
2024-05-28 00:38:42 +0900

    fix: 매너점수 프로퍼티 수정

--- src/components/common/MyPageUserInfo/MyPageUserInfo.tsx
+++ src/components/common/MyPageUserInfo/MyPageUserInfo.tsx
@@ -1,23 +1,29 @@
 import { S } from './MyPageUserInfo.style.ts'
 import { FriendRequest } from '../../../types/friendRequest.ts'
 import { Participant } from '../../ParticipationGameList/ParticipationGameList.tsx'
+import {
+  convertAttributeToKorean,
+  convertGenderToKorean,
+  convertPlayStyleToKorean,
+} from '../../../helper/convertValueToName.ts'
 
 export default function MyPageUserInfo({
   userInfo,
 }: {
   userInfo: FriendRequest | Participant
 }) {
+  console.log(userInfo)
+
   return (
     <div>
       <S.Wrapper>
         <S.ListBox>
-          <S.ListItem key={userInfo.userId}>
-            <p className='nickname'>{userInfo.nickName}</p>
-            {/*{userInfo.mannerPoint}*/}
-            {/*<p className='rating'>{userInfo.score}</p>*/}
-            <p>{userInfo.gender}</p>
-            <p>{userInfo.playStyle}</p>
-            <p>{userInfo.ability}</p>
+          <S.ListItem key={userInfo?.userId}>
+            <p className='nickname'>{userInfo?.nickName}</p>
+            <p className='rating'>{userInfo?.mannerPoint || '0.0'}</p>
+            <p>{convertGenderToKorean(userInfo?.gender)}</p>
+            <p>{convertPlayStyleToKorean(userInfo?.playStyle)}</p>
+            <p>{convertAttributeToKorean(userInfo?.ability)}</p>
           </S.ListItem>
         </S.ListBox>
       </S.Wrapper>
