86c837e88e7293683a3eefec85103892586add72
osw6858 <osw7875@naver.com>
2024-05-29 00:42:31 +0900

    fix: 소셜 로그인시 헤더 아이콘 변경이 안되는 문제 해결 및 메인 페이지 날짜 문제 수정

--- src/components/OAuthRedirect/OAuthRedirect.tsx
+++ src/components/OAuthRedirect/OAuthRedirect.tsx
@@ -1,9 +1,11 @@
 import { useEffect } from 'react'
 import { defaultAxios } from '../../api/axiosInstance.ts'
 import { useNavigate, useSearchParams } from 'react-router-dom'
+import { useAuthStore } from '../../store/store.ts'
 
 export function OAuthRedirect() {
   const navigate = useNavigate()
+  const setLoginState = useAuthStore((state) => state.login)
 
   const [searchParams] = useSearchParams()
   const code = searchParams.get('code')
@@ -20,6 +22,7 @@ export function OAuthRedirect() {
         localStorage.setItem('Access-Token', accessToken)
         localStorage.setItem('Refresh-Token', refreshToken)
         localStorage.setItem('userPK', String(res.data.userInfo.userId))
+        setLoginState()
       } catch (error) {
         console.error('Login failed:', error)
       } finally {
