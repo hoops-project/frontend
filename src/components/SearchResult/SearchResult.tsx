36c977b766932803a31538144c15c70e09131493
Pinn <lsol1130@naver.com>
2024-05-29 18:13:56 +0900

    feat: 친구 신청 및 삭제기능 구현

--- src/components/SearchResult/SearchResult.tsx
+++ src/components/SearchResult/SearchResult.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import { useSearchFriendsQuery } from '../../hooks/query/useSearchFriendsQuery.ts';
-import { axiosAuth } from '../../api/axiosInstance.ts';
 import { S } from './SearchResult.style.ts';
-import { END_POINT } from '../../constants/endPoint.ts';
 import { theme } from '../../styles/theme.ts';
 import { UserInfoType } from '../../types/auth.ts';
-
 import BasicButton from '../common/BasicButton/BasicButton.tsx';
+import useFriendActions from '../../hooks/query/useFriendActions.ts';
 
 interface SearchFriendListProps {
   query: string;
@@ -14,43 +12,57 @@ interface SearchFriendListProps {
 
 const SearchFriendList: React.FC<SearchFriendListProps> = ({ query }) => {
   const { searchResult } = useSearchFriendsQuery(query);
+  const { handleAddFriend, handleRemoveFriend, addedFriends, removedFriends } = useFriendActions();
 
-  const handleAddFriend = async (friendId: number) => {
-    try {
-      const response = await axiosAuth.post(`${END_POINT.FRIENDS.APPLY_FRIEND}`, { friendId });
-      console.log('Friend added:', response.data);
-      // 데이터 갱신 로직 추가 가능
-    } catch (error) {
-      console.error('Error adding friend:', error);
-    }
-  };
   const friends = searchResult?.content ?? [];
   console.log(friends);
 
-
   return (
-    query ? <S.ListBox>
-      {friends.map((user: UserInfoType) => (
-        <S.ListItem key={user.userId}>
-          <p>{user.nickName}</p>
-          <p>{user.rating}</p>
-          <p>{user.gender}</p>
-          <p>{user.playStyle}</p>
-          <p>{user.ability}</p>
-          <BasicButton
-            type="button"
-            $bgColor={user.friendId === null ? theme.colors.blue : theme.colors.red}
-            $borderColor={theme.colors.white}
-            $width="10rem"
-            $hoverBgColor={theme.colors.gray_300}
-            $fontcolor={theme.colors.white}
-            onClick={() => handleAddFriend(user.userId)}
-          >
-            {user.friendId === null ? '친구 신청' : '친구 삭제'}
-          </BasicButton>
-        </S.ListItem>
-      ))}
-    </S.ListBox>: null
+    query ? (
+      <S.ListBox>
+        {friends.map((user: UserInfoType) => {
+          const isAdded = addedFriends.includes(user.userId);
+          const isRemoved = removedFriends.includes(user.userId);
+          const isFriend = user.friendId !== null;
+
+          return (
+            <S.ListItem key={user.userId}>
+              <p>{user.nickName}</p>
+              <p>{user.rating}</p>
+              <p>{user.gender}</p>
+              <p>{user.playStyle}</p>
+              <p>{user.ability}</p>
+              <BasicButton
+                type="button"
+                $bgColor={
+                  isAdded
+                    ? theme.colors.green
+                    : isRemoved
+                    ? theme.colors.gray_300
+                    : isFriend
+                    ? theme.colors.red
+                    : theme.colors.blue
+                }
+                $borderColor={theme.colors.white}
+                $width="10rem"
+                $hoverBgColor={theme.colors.gray_300}
+                $fontcolor={theme.colors.white}
+                onClick={() => (isFriend ? handleRemoveFriend(user.userId) : handleAddFriend(user.userId))}
+                disabled={isAdded || isRemoved}
+              >
+                {isAdded
+                  ? '신청 완료'
+                  : isRemoved
+                  ? '삭제 완료'
+                  : isFriend
+                  ? '친구 삭제'
+                  : '친구 신청'}
+              </BasicButton>
+            </S.ListItem>
+          );
+        })}
+      </S.ListBox>
+    ) : null
   );
 };
 
