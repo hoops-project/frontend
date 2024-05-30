9ba68d4c0ffedced11738026924320be808f0b64
osw6858 <osw7875@naver.com>
2024-05-30 14:54:26 +0900

    fix: MSW 주석 해제

--- src/main.tsx
+++ src/main.tsx
@@ -8,24 +8,23 @@ import { queryClient } from './api/QueryClient'
 import { ThemeProvider } from 'styled-components'
 import { theme } from './styles/theme'
 
-// MSW 설정을 제외합니다.
-// async function enableMocking() {
-//   if (import.meta.env.DEV) {
-//     // MSW 설정을 비동기로 로드
-//     const { worker } = await import('./mock/setupWorker');
-//     return worker.start({ onUnhandledRequest: 'bypass' });
-//   }
-// }
+async function enableMocking() {
+  if (import.meta.env.DEV) {
+    // MSW 설정을 비동기로 로드
+    const { worker } = await import('./mock/setupWorker')
+    return worker.start({ onUnhandledRequest: 'bypass' })
+  }
+}
 
-// enableMocking().then(() => {
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <QueryClientProvider client={queryClient}>
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </ThemeProvider>
-  </QueryClientProvider>
-)
-// });
+enableMocking().then(() => {
+  ReactDOM.createRoot(document.getElementById('root')!).render(
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </ThemeProvider>
+    </QueryClientProvider>
+  )
+})
