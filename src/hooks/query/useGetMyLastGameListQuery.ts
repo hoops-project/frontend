cfd12b95b804639344962f50b90c59ab7f94d4c7
osw6858 <osw7875@naver.com>
2024-05-30 11:33:59 +0900

    fix: 지난경기 불러올때 한번에 불러오는 size 수 변경

--- src/hooks/query/useGetMyLastGameListQuery.ts
+++ src/hooks/query/useGetMyLastGameListQuery.ts
@@ -2,21 +2,21 @@ import { axiosAuth } from '../../api/axiosInstance.ts'
 import { END_POINT } from '../../constants/endPoint.ts'
 import { useInfiniteQuery, useQueryClient } from '@tanstack/react-query'
 import { QUERY_KEYS } from '../../constants/queryKeys.ts'
-import { useEffect } from 'react';
+import { useEffect } from 'react'
 
 const fetchAPI = async (page: number, size: number) => {
   const res = await axiosAuth.get(END_POINT.GAME_USER.PAST_GAME_LIST, {
     params: {
       page,
-      size
+      size,
     },
-  });
-  return res.data;
-};
+  })
+  return res.data
+}
 
 const useGetMyLastGameListQuery = () => {
-  const queryClient = useQueryClient();
-  
+  const queryClient = useQueryClient()
+
   const {
     data: myLastGameListData,
     fetchNextPage: myLastGameListFetchNextPage,
@@ -26,17 +26,17 @@ const useGetMyLastGameListQuery = () => {
     isFetching: myLastGameListIsFetching,
   } = useInfiniteQuery({
     queryKey: [QUERY_KEYS.GET_MY_LAST_GAME],
-    queryFn: ({ pageParam = 1 }) => fetchAPI(pageParam, 1),
+    queryFn: ({ pageParam = 1 }) => fetchAPI(pageParam, 10),
     getNextPageParam: (lastPage) => {
-      const nextPage = lastPage.pageable.pageNumber + 2;
-      return lastPage.last ? undefined : nextPage;
+      const nextPage = lastPage.pageable.pageNumber + 2
+      return lastPage.last ? undefined : nextPage
     },
     initialPageParam: 1,
-  });
+  })
 
   useEffect(() => {
-    queryClient.invalidateQueries({ queryKey: [QUERY_KEYS.GET_MY_LAST_GAME] });
-  }, [queryClient]);
+    queryClient.invalidateQueries({ queryKey: [QUERY_KEYS.GET_MY_LAST_GAME] })
+  }, [queryClient])
 
   return {
     myLastGameListData,
@@ -45,7 +45,7 @@ const useGetMyLastGameListQuery = () => {
     myLastGameListRefetch,
     myLastGameListIsLoading,
     myLastGameListIsFetching,
-  };
-};
+  }
+}
 
-export default useGetMyLastGameListQuery;
+export default useGetMyLastGameListQuery
