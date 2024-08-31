3e6b6a96d58c9c36ca41fe91ce72191870e53d71
osw6858 <osw7875@naver.com>
2024-08-31 19:53:06 +0900

    fix: 회원가입 중복검사 버그 및 경기참여 에러처리 버그 수정

--- src/hooks/query/useJoinGameQuery.ts
+++ src/hooks/query/useJoinGameQuery.ts
@@ -1,30 +1,37 @@
-import { useMutation } from "@tanstack/react-query";
-import { useNavigate } from "react-router-dom";
-import { axiosAccess } from "../../api/axiosInstance";
-import { END_POINT } from "../../constants/endPoint";
-import useToast from "../useToast";
-import { QUERY_KEYS } from "../../constants/queryKeys";
+import { useMutation } from '@tanstack/react-query'
+import { useNavigate } from 'react-router-dom'
+import { axiosAuth } from '../../api/axiosInstance'
+import { END_POINT } from '../../constants/endPoint'
+import useToast from '../useToast'
+import { QUERY_KEYS } from '../../constants/queryKeys'
+import { AxiosError } from 'axios'
 
 const fetchAPI = async (gameId: string) => {
-  const res = await axiosAccess.post(`${END_POINT.GAME_USER.GAME_IN_OUT}`,{ gameId });
-  return res.data;
-};
+  const res = await axiosAuth.post(`${END_POINT.GAME_USER.GAME_IN_OUT}`, {
+    gameId,
+  })
+  return res.data
+}
 
 const useJoinGameQuery = () => {
-  const navigate = useNavigate();
-  const { toastSuccess } = useToast();
-
+  const navigate = useNavigate()
+  const { toastSuccess, toastError } = useToast()
 
   const { mutate: joinGame } = useMutation({
     mutationKey: [QUERY_KEYS.JOIN_GAME],
     mutationFn: (gameId: string) => fetchAPI(gameId),
-    onSuccess: (_data,variables) => {
-      toastSuccess("경기 참가 신청이 완료되었습니다🏀");
-      navigate(`/detail/${variables}`);
+    onSuccess: (_data, variables) => {
+      toastSuccess('경기 참가 신청이 완료되었습니다🏀')
+      navigate(`/detail/${variables}`)
     },
-  });
+    onError: (error) => {
+      if (error instanceof AxiosError) {
+        toastError(error.response?.data.errorMessage)
+      }
+    },
+  })
 
-  return { joinGame };
-};
+  return { joinGame }
+}
 
-export default useJoinGameQuery;
+export default useJoinGameQuery
